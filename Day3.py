a8b61b4baec3586cc3856af13c8eb614a8ac0b6c
Sayyed Muntzr <muntzarsayyed@gmail.com>
2023-08-16 01:19:09 +0530

    Update Day3.py

--- Day3.py
+++ Day3.py
@@ -14,4 +14,49 @@ else:
     print("Not leap year.")
 
 
+# Pizza Order Practice 
+# Problem Statement
+# Your first job is to build an automatic pizza order program.
+# Based on a user's order, work out their final bill.
+# Small Pizza: $15
+# Medium Pizza: $20
+# Large Pizza: $25
+# Pepperoni for Small Pizza: +$2
+# Pepperoni for Medium or Large Pizza: +$3
+# Extra cheese for any size pizza: + $1
 
+
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+bill = 0
+
+if size == 'S':
+    bill+=15
+    # print("Your final bill is: $15")
+elif size=='M':
+    bill+=20
+    # print("Your final bill is: $20")
+elif size=='L':
+    bill+=25
+    # print("Your final bill is: $25")
+
+if add_pepperoni=='Y':
+    if size=='S':
+        bill= bill +2
+    else:
+        bill= bill + 3
+# print(f"your final bill is: {bill}")
+
+if extra_cheese=='Y':
+    bill+=1
+
+print(f"Your final bill is: ${bill}")
+
+# else:
+#     print(f"Your final bill is : ${bill}")
