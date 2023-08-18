c9e27b41659df8930f37a0cbcca0e5afd9043102
Sayyed Muntzr <muntzarsayyed@gmail.com>
2023-08-19 03:08:15 +0530

    Update Day3.py

--- Day3.py
+++ Day3.py
@@ -96,7 +96,65 @@ elif love_score>=40 and love_score<=50:
     print(f"Your score is {love_score}, you are alright together.")
 else:
     print(f"Your score is {love_score}")
-        
+
+
+
+
+
+Task: Treasure Island.
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+
+
+cross=input("you are at coss road.Where do you want to go ? left or right? \n ").lower()
+if cross == "right":
+  print("you fell into hell.Game Over")
+else:
+  cross=="left"
+  next_pass=input('You\'ve come to lake. This is an island. Type "wait" to wait for boat or type "swim" to swim. \n').lower()
+  if next_pass=="swim":
+    print("You are attacked by trout. Game over")
+  else:
+    next=input("From which door you want to go ? 'red', 'blue','yellow' ? \n").lower()
+    if next=="red":
+      print("Burned by fire. Game Over. ")
+    elif next=="blue":
+      print("Eaten by beasts.Game Over.")
+    elif next=="yellow":
+      print("You Win.")
+    else:
+      print("Game Over")
+    
+  
+  
+  
 
 
 
