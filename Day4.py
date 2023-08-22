edb3acd356b74d7a9ab7d39bbb20aa395309b95e
Sayyed Muntzar <muntzarsayyed@gmail.com>
2023-08-22 15:13:46 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -1,1 +1,12 @@
+Random Bill Payer Generator
+# Import the random module here
+import random
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
 
+#Write your code below this line 👇
+# print(names)
+random_name= random.choice(names)
+print(f"{random_name} is going to buy the meal today!")
