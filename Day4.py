35c138b864003fb99e1ee5912ce8589c13dd4406
Sayyed Muntzar <muntzarsayyed@gmail.com>
2023-08-24 00:09:16 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -10,3 +10,17 @@ names = names_string.split(", ")
 # print(names)
 random_name= random.choice(names)
 print(f"{random_name} is going to buy the meal today!")
+
+Treasure Map
+
+# Import the random module here
+import random
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+# print(names)
+random_name= random.choice(names)
+print(f"{random_name} is going to buy the meal today!")
