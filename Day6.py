a1175074e44e97d1de60f3dfae7739c495e55011
Sayyed Muntzar <muntzarsayyed@gmail.com>
2023-09-09 17:17:22 +0530

    Update Day6.py

--- Day6.py
+++ Day6.py
@@ -1,1 +1,28 @@
+def move_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def first_huddle():
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+
+def for_left():
+    move()
+    turn_left()
+    move()
+
+def passing():
+    for_left()
+    move_right()
+    move()
+    first_huddle()
+    turn_left()
+
+for step in range(6):
+    passing()
+  
+
 
