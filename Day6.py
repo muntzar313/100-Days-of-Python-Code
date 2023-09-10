85edc9f3ba7b4abcf47726de5a5acd7e6265fb5b
Sayyed Muntzar <muntzarsayyed@gmail.com>
2023-09-10 20:50:59 +0530

    Update Day6.py

--- Day6.py
+++ Day6.py
@@ -1,3 +1,5 @@
+# Using For Loop
+
 def move_right():
     turn_left()
     turn_left()
@@ -24,5 +26,33 @@ def passing():
 for step in range(6):
     passing()
   
+## Using WHile Loop
+def move_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def first_huddle():
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+
+def for_left():
+    move()
+    turn_left()
+    move()
+
+def passing():
+    for_left()
+    move_right()
+    move()
+    first_huddle()
+    turn_left()
+
+while not at_goal():
+    passing()
+
+
 
 
